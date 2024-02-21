b8be5afd82bfe421b28bd7a8ab6e399ebc01c0b5
Gautam Adhikari <gautamadhikari831@gmail.com>
2024-02-22 00:02:47 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version (With strict mode, you can not, for example, use undeclared variables.)
+// x = 3.14; //error
+//console.log(x);
+//alert(3+3) //cannot use in nodejs (can use in browser)
+//original documentation tc39.es
+
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Gautam")
+
+
+let name = "gautam"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
