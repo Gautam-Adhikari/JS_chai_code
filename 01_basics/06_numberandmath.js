e63dac19392b03cf83dbb5dc64d4d792a465b24e
Gautam Adhikari <gautamadhikari831@gmail.com>
2024-03-08 23:27:35 +0530

    revise

--- 01_basics/06_numberandmath.js
+++ 01_basics/06_numberandmath.js
@@ -2,14 +2,16 @@ const score = 400
 // console.log(score);
 
 const balance = new Number(100)
+
+// console.log(balance.toFixed(1)); //after decimal places
 // console.log(balance);
 
 // console.log(balance.toString().length);
-// console.log(balance.toFixed(1)); //after decimal places
+
 
 const otherNumber = 123.8966
 
-// console.log(otherNumber.toPrecision(6));
+console.log(otherNumber.toPrecision(6));
 
 const hundreds = 1000000
 // console.log(hundreds.toLocaleString('en-IN'));
